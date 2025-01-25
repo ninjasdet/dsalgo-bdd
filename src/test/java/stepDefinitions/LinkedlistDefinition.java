a4d7c34b8c97e95598ac096949cc3a3dcc8a3a34
ramyaninja <ramyatech7@gmail.com>
2025-01-24 21:09:14 -0500

    Adding Register,Array,Linked List

---
+++ src/test/java/stepDefinitions/LinkedlistDefinition.java
@@ -0,0 +1,40 @@
+package stepDefinitions;
+import io.cucumber.java.en.Given;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LinkedlistDefinition {
+	@When("The user clicks the {string} button in Linked List")
+	public void the_user_clicks_the_button_in_linked_list(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("The user clicks {string} HyperLink")
+	public void the_user_clicks_hyper_link(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The user  be redirected to {string} page")
+	public void the_user_be_redirected_to_page(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("The user is on the {string} traversal page")
+	public void the_user_is_on_the_traversal_page(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user clicks the Run Button without entering the code in the Editor")
+	public void the_user_clicks_the_run_button_without_entering_the_code_in_the_editor() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+
+
+}
