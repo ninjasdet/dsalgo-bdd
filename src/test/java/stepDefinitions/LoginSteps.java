c6abfcb7fc247a350753516f667371b344dc9b74
Gituser-See <seethal .sajith@gmail.com>
2025-01-24 19:14:09 -0600

    adding Homepage,loginpage,stack,queue related files

---
+++ src/test/java/stepDefinitions/LoginSteps.java
@@ -0,0 +1,61 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LoginSteps {
+
+
+@Given("The user is on the DS Algo Home Page")
+public void the_user_is_on_the_ds_algo_home_page() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@When("The user clicks the Sign in link")
+public void the_user_clicks_the_sign_in_link() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@Then("The user should be redirected to the Sign in page")
+public void the_user_should_be_redirected_to_the_sign_in_page() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@Given("The user is on the DS Algo Sign in Page")
+public void the_user_is_on_the_ds_algo_sign_in_page() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@When("The user clicks the login button after entering {int} from  {string}")
+public void the_user_clicks_the_login_button_after_entering_from(Integer int1, String string) {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@Then("The error message Please fill out this field will be displayed")
+public void the_error_message_please_fill_out_this_field_will_be_displayed() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@Then("The error message Invalid username and password will be displayed")
+public void the_error_message_invalid_username_and_password_will_be_displayed() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@Then("The success message You are logged in will be displayed")
+public void the_success_message_you_are_logged_in_will_be_displayed() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+
+
+
+}
